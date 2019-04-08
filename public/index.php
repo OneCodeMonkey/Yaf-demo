395a0aa7aa1c042065100ebda59d8c69be6d0220
Jax-leo <1460018362@qq.com>
2019-04-09 01:12:00 +0800

    modify public/index.php

--- public/index.php
+++ public/index.php
@@ -4,4 +4,3 @@ header('content-type:text/html;charset=utf-8');
 define('APP_PATH', realpath(dirname(__FILE__) . '/../')); // APP_PATH 即为 yaf 目录本身
 $app = new Yaf\Application(APP_PATH . "/conf/application.ini");
 $app->bootstrap()->run();
-?>
