70a14366a8529f9ef8a8ab16c06d288f185e5da7
Jax-leo <1460018362@qq.com>
2019-04-09 02:45:28 +0800

    add controller: index

---
+++ application/controllers/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @name IndexController
+ * @desc 默认控制器
+ * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
+ */
+class IndexController extends \Yaf\Controller_Abstract
+{
+	public function indexAction ()
+	{
+		$this->getView()->assign('content', 'Hello Yaf!');
+	}
+}
