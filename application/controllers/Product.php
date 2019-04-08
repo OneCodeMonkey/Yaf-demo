e0b8536aa785efc440a944f5787f10c8e3f695c8
Jax-leo <1460018362@qq.com>
2019-04-09 02:46:20 +0800

    add controller: product

---
+++ application/controllers/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\Product;
+
+class ProductController extends \Yaf\Controller_Abstract
+{
+	public function indexAction ()
+	{
+		// Eloquent ORM调用方式
+		$products = Product::all();
+
+		$this->getView()->assign('products', $products);
+	}
+}
